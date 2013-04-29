57aa668903b0f99cb8a9a17c7ea994772299f141
debian <debian@aspire.localdomain>
2013-04-29 21:16:27 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Todolist::Application.config.secret_token = 'bdd1e74e1ca44ef96abca103dca29e6dea63d3d40cd9529c6188f2c8060233abc69e7b38cc3a83b2f1789f8e0b68fd590af60c703c48f3d864e26f0932d198fb'
